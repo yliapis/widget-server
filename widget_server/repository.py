41c62a0006c583ce27baa3c23c32b561c35eb255
yliapis <yliapis44@gmail.com>
2024-01-13 05:12:32 -0600

    refactor widget managment to repository class

--- widget_server/repository.py
+++ widget_server/repository.py
@@ -13,15 +13,38 @@ class Widget(BaseModel):
 # Volatile internal widget store
 # TODO: Replace with a persistent store
 
-widget_store_table: dict[str, Widget] = {}
+_widget_store_table: dict[str, Widget] = {}
 
 
-def create_widget(name: str) -> Widget:
-    return Widget(
-        id=str(uuid.uuid4()),
-        name=name,
-    )
+class WidgetRepository:
+    def create_widget(self, name: str) -> Widget:
+        new_widget = Widget(
+            id=str(uuid.uuid4()),
+            name=name,
+        )
+        _widget_store_table[new_widget.id] = new_widget
+        return new_widget
 
+    def get_widget(self, id: str) -> Widget | None:
+        print("id: ", id)
+        print("_widget_store_table: ", _widget_store_table)
+        return _widget_store_table.get(id, None)
 
-def get_widget(id: str) -> Widget | None:
-    return widget_store_table.get(id)
+    def get_all_widgets(self) -> list[Widget]:
+        return list(_widget_store_table.values())
+
+    def update_widget(self, id: str, name: str) -> Widget | None:
+        if id not in _widget_store_table:
+            return None
+        _widget_store_table[id].name = name
+        return _widget_store_table[id]
+
+    def delete_widget(self, id: str) -> bool:
+        if id not in _widget_store_table:
+            return False
+        del _widget_store_table[id]
+        return True
+
+    def delete_all_widgets(self) -> bool:
+        _widget_store_table.clear()
+        return True
