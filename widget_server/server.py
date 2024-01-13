308e0acb795bd06470daf6f305a8ee034616242b
yliapis <yliapis44@gmail.com>
2024-01-13 04:11:07 -0600

    add cors middleware to server to share routes with frontend

--- widget_server/server.py
+++ widget_server/server.py
@@ -1,7 +1,25 @@
 # this needs to be imported to start the app
 from widget_server.app import app  # noqa: F401
 
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+
+
+# these are the origins that are allowed to access the API;
+# these origins will come from the frontend run by node
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(
