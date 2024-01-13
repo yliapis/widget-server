e36f95071e91b8d01b6dbdb8fc87ee02a582f049
yliapis <yliapis44@gmail.com>
2024-01-13 11:24:34 -0600

    create rudimentary create and read frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+const BASE_URL = "http://localhost:8000"
+
+interface Widget {
+  id: string;
+  name: string;
+}
+
+function CreateWidgetInputForm() {
+  const [widgetName, setWidgetName] = useState('');
+  const [widgets, setWidgets] = useState<Widget[]>([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + "/widgets")
+      .then(response => response.json())
+      .then(data => setWidgets(data.widgets));
+  }, []);
+
+  const clickHandler = () => {
+    console.log(widgetName);
+    fetch(BASE_URL + "/widgets/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": widgetName
+      })
+    });
+  };
+
+  return (
+    <div>
+      <span>
+        <input type="text" id="fname" name="fname" value={widgetName} onChange={(e) => setWidgetName(e.target.value)} />
+        <input type="submit" value="Create Widget" onClick={clickHandler} />
+      </span>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {widgets.map(widget => (
+            <tr key={widget.id}>
+              <td>{widget.id}</td>
+              <td>{widget.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <CreateWidgetInputForm />
       </header>
     </div>
   );
